f2b8470a9348101835e1e032e9b2e64fcc0ac6a2
Robert J. Macomber <robert.macomber@socrata.com>
2012-10-18 16:16:26 -0700

    MOAR FUNCTIONS

--- soql-analysis/src/main/scala/com/socrata/soql/analysis/types/SoQLFunctions.scala
+++ soql-analysis/src/main/scala/com/socrata/soql/analysis/types/SoQLFunctions.scala
@@ -9,7 +9,12 @@ object SoQLFunctions {
   val TextToFixedTimestamp = new MonomorphicFunction(FunctionName("to_fixed_timestamp"), Seq(SoQLText), SoQLFixedTimestamp).function
   val TextToFloatingTimestamp = new MonomorphicFunction(FunctionName("to_floating_timestamp"), Seq(SoQLText), SoQLFloatingTimestamp).function
   val Concat = Function(SpecialFunctions.Operator("||"), Seq(VariableType("a"), VariableType("b")), FixedType(SoQLText))
+  val Gte = Function(SpecialFunctions.Operator(">="), Seq(VariableType("a"), VariableType("a")), FixedType(SoQLBoolean))
   val Gt = Function(SpecialFunctions.Operator(">"), Seq(VariableType("a"), VariableType("a")), FixedType(SoQLBoolean))
+  val Lt = Function(SpecialFunctions.Operator("<"), Seq(VariableType("a"), VariableType("a")), FixedType(SoQLBoolean))
+  val Lte = Function(SpecialFunctions.Operator("<="), Seq(VariableType("a"), VariableType("a")), FixedType(SoQLBoolean))
+  val Eq = Function(SpecialFunctions.Operator("="), Seq(VariableType("a"), VariableType("a")), FixedType(SoQLBoolean))
+  val Neq = Function(SpecialFunctions.Operator("<>"), Seq(VariableType("a"), VariableType("a")), FixedType(SoQLBoolean))
   val LatitudeField = new MonomorphicFunction(SpecialFunctions.Subscript, Seq(SoQLLocation, SoQLTextLiteral("latitude")), SoQLDouble).function
   val LongitudeField = new MonomorphicFunction(SpecialFunctions.Subscript, Seq(SoQLLocation, SoQLTextLiteral("longitude")), SoQLDouble).function
   val IsNotNull = Function(SpecialFunctions.IsNotNull, Seq(VariableType("a")), FixedType(SoQLBoolean))
@@ -17,6 +22,20 @@ object SoQLFunctions {
   val CountStar = Function(SpecialFunctions.StarFunc("count"), Seq(), FixedType(SoQLNumber), isAggregate = true)
 
   val PlusNumNum = new MonomorphicFunction(SpecialFunctions.Operator("+"), Seq(SoQLNumber, SoQLNumber), SoQLNumber).function
+  val PlusMoneyMoney = new MonomorphicFunction(SpecialFunctions.Operator("+"), Seq(SoQLMoney, SoQLMoney), SoQLMoney).function
+  val PlusDoubleDouble = new MonomorphicFunction(SpecialFunctions.Operator("+"), Seq(SoQLDouble, SoQLDouble), SoQLDouble).function
+
+  val TimesNumNum = new MonomorphicFunction(SpecialFunctions.Operator("*"), Seq(SoQLNumber, SoQLNumber), SoQLNumber).function
+  val TimesDoubleDouble = new MonomorphicFunction(SpecialFunctions.Operator("*"), Seq(SoQLDouble, SoQLDouble), SoQLDouble).function
+  val TimesNumMoney = new MonomorphicFunction(SpecialFunctions.Operator("*"), Seq(SoQLNumber, SoQLMoney), SoQLMoney).function
+  val TimesMoneyNum = new MonomorphicFunction(SpecialFunctions.Operator("*"), Seq(SoQLMoney, SoQLNumber), SoQLMoney).function
+
+  val NumberToMoney = new MonomorphicFunction(SpecialFunctions.Operator("to_money"), Seq(SoQLNumber), SoQLMoney).function
+  val NumberToDouble = new MonomorphicFunction(SpecialFunctions.Operator("to_double"), Seq(SoQLNumber), SoQLDouble).function
+
+  val And = new MonomorphicFunction(FunctionName("and"), Seq(SoQLBoolean, SoQLBoolean), SoQLBoolean).function
+  val Or = new MonomorphicFunction(FunctionName("or"), Seq(SoQLBoolean, SoQLBoolean), SoQLBoolean).function
+  val Not = new MonomorphicFunction(FunctionName("not"), Seq(SoQLBoolean), SoQLBoolean).function
 
   lazy val allFunctions = {
     for {
