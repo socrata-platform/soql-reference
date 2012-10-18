11b8e8bed44e8c776fc9e2b1c6eebf288979f39d
Robert J. Macomber <robert.macomber@socrata.com>
2012-10-18 15:08:33 -0700

    Position typedFFs correctly

--- soql-analysis/src/main/scala/com/socrata/soql/analysis/Typechecker.scala
+++ soql-analysis/src/main/scala/com/socrata/soql/analysis/Typechecker.scala
@@ -1,5 +1,7 @@
 package com.socrata.soql.analysis
 
+import scala.util.parsing.input.{NoPosition, Position}
+
 import com.socrata.soql.ast._
 import com.socrata.soql.names._
 import util.parsing.input.Position
@@ -20,24 +22,24 @@ abstract class Typechecker[Type] { self =>
   def apply(e: Expression) = typecheck(e.removeParens)
 
   private def typecheck(e: Expression): typed.TypedFF[Type] = e match {
-    case ColumnOrAliasRef(col) =>
+    case r@ColumnOrAliasRef(col) =>
       aliases.get(col) match {
         case Some(tree) =>
           tree
         case None =>
           columns.get(col) match {
             case Some(typ) =>
-              p(typed.ColumnRef(col, typ), e)
+              typed.ColumnRef(col, typ).positionedAt(r.position)
             case None =>
-              throw new UnknownColumn(e.position)
+              throw new UnknownColumn(r.position)
           }
       }
     case c@Cast(expr, targetTypeName) =>
       val typedExpr = typecheck(expr)
       val targetType = typeFor(targetTypeName, c.targetTypePosition)
       val cast = getCastFunction(typedExpr.typ, targetType, c.targetTypePosition)
-      typed.FunctionCall(cast, Seq(typedExpr))
-    case FunctionCall(name, parameters) =>
+      typed.FunctionCall(cast, Seq(typedExpr)).positionedAt(c.position).functionNameAt(c.operatorPosition)
+    case fc@FunctionCall(name, parameters) =>
       val typedParameters = parameters.map(typecheck(_))
 
       val options = functionsWithArity(name, typedParameters.length, e.position)
@@ -46,28 +48,23 @@ abstract class Typechecker[Type] { self =>
           val realParameterList = (typedParameters, cs).zipped.map { (param, converter) =>
             converter match {
               case None => param
-              case Some(conv) => typed.FunctionCall(conv, Seq(param))
+              case Some(conv) => typed.FunctionCall(conv, Seq(param)).positionedAt(NoPosition).functionNameAt(NoPosition)
             }
           }
-          typed.FunctionCall(f, realParameterList.toSeq)
+          typed.FunctionCall(f, realParameterList.toSeq).positionedAt(fc.position).functionNameAt(fc.functionNamePosition)
         case Ambiguous(_) | NoMatch =>
           // when reporting this, remember to convert special functions back to their syntactic form
           // also TODO: better error reporting in the "no match" case
           error("nyi")
       }
-    case BooleanLiteral(b) =>
-      typed.BooleanLiteral(b, booleanLiteralType(b))
-    case StringLiteral(s) =>
-      typed.StringLiteral(s, stringLiteralType(s))
-    case NumberLiteral(n) =>
-      typed.NumberLiteral(n, numberLiteralType(n))
-    case NullLiteral() =>
-      typed.NullLiteral(nullLiteralType)
-  }
-
-  private def p(tff: typed.TypedFF[Type], uff: Expression) = {
-    tff.position = uff.position
-    tff
+    case bl@BooleanLiteral(b) =>
+      typed.BooleanLiteral(b, booleanLiteralType(b)).positionedAt(bl.position)
+    case sl@StringLiteral(s) =>
+      typed.StringLiteral(s, stringLiteralType(s)).positionedAt(sl.position)
+    case nl@NumberLiteral(n) =>
+      typed.NumberLiteral(n, numberLiteralType(n)).positionedAt(nl.position)
+    case nl@NullLiteral() =>
+      typed.NullLiteral(nullLiteralType).positionedAt(nl.position)
   }
 
   def booleanLiteralType(b: Boolean): Type
