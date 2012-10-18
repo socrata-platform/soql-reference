67555b9b6f0987d7f35176ec2a2dc79234ffa298
Robert J. Macomber <robert.macomber@socrata.com>
2012-10-18 15:09:55 -0700

    Rework AggregateChecker to have only one method for general use

--- soql-analysis/src/main/scala/com/socrata/soql/analysis/AggregateChecker.scala
+++ soql-analysis/src/main/scala/com/socrata/soql/analysis/AggregateChecker.scala
@@ -5,59 +5,74 @@ import scala.util.parsing.input.Position
 import com.socrata.soql.names.{ColumnName, FunctionName}
 import com.socrata.soql.analysis.typed._
 
-class AggregateInUngroupedContext(val function: FunctionName, val position: Position) extends Exception("Cannot use an aggregate function here:\n" + position.longString)
+class AggregateInUngroupedContext(val function: FunctionName, clause: String, val position: Position) extends Exception("Cannot use an aggregate function in " + clause + ":\n" + position.longString)
 class ColumnNotInGroupBys(val column: ColumnName, val position: Position) extends Exception("Column not in group bys:\n" + position.longString)
 
-class AggregateChecker[Type](aggregates: Set[MonomorphicFunction[Type]]) {
+class AggregateChecker[Type] {
   type Expr = TypedFF[Type]
 
   /** Check that aggregates and column-names are used as appropriate for
     * the query.
     *
-    * @param where The WHERE clause for the query, if present
-    * @param orderBy The ORDER BY expressions for the query, minus sorting options
     * @param outputs The selected expressions for the query
-    * @return `true` if this really was a groupless query or `false` if it's grouped by the empty list.
+    * @param where The WHERE clause for the query, if present
+    * @param groupBy The GROUP BY expressions, if the clause is present
+    * @param having The HAVING clause for the query, if present
+    * @param orderBy The ORDER BY expressions for the query, minus sorting options
+    * @return `true` if this was a grouped query or `false` otherwise.
     */
-  def apply(where: Option[Expr], orderBy: Seq[Expr], outputs: Seq[Expr]): Boolean = {
-    where.foreach(checkPregroupExpression)
-    try {
-      orderBy.foreach(checkPregroupExpression)
-      outputs.foreach(checkPregroupExpression)
+  def apply(outputs: Seq[Expr], where: Option[Expr], groupBy: Option[Seq[Expr]], having: Option[Expr], orderBy: Seq[Expr]): Boolean = {
+    if(groupBy.isDefined || having.isDefined) { // ok, definitely a grouped query
+      checkGrouped(outputs, where, groupBy.getOrElse(Nil), having, orderBy)
       true
-    } catch {
-      case _: AggregateInUngroupedContext => // this is actually "group by {empty list}"
-        apply(where, Nil, None, orderBy, outputs)
-        false
+    } else {
+      // neither GROUP BY nor HAVING are set.  It could still be a grouped
+      // query if there are aggregate calls in outputs or order by though...
+      try {
+        outputs.foreach(checkPregroupExpression("selected columns", _))
+      } catch {
+        case _: AggregateInUngroupedContext =>
+          checkGrouped(outputs, where, Nil, None, orderBy)
+          return true
+      }
+      where.foreach(checkPregroupExpression("WHERE", _))
+      try {
+        orderBy.foreach(checkPregroupExpression("ORDER BY", _))
+      } catch {
+        case _: AggregateInUngroupedContext =>
+          checkGrouped(outputs, where, Nil, None, orderBy)
+          return true
+      }
+      false
     }
   }
 
-  def apply(where: Option[Expr], groupBy: Seq[Expr], having: Option[Expr], orderBy: Seq[Expr], outputs: Seq[Expr]) {
-    where.foreach(checkPregroupExpression)
-    groupBy.foreach(checkPregroupExpression)
-    having.foreach(checkPostgroupExpression(_, groupBy))
-    orderBy.foreach(checkPostgroupExpression(_, groupBy))
-    outputs.foreach(checkPostgroupExpression(_, groupBy))
+  def checkGrouped(outputs: Seq[Expr], where: Option[Expr], groupBy: Seq[Expr], having: Option[Expr], orderBy: Seq[Expr]) {
+    outputs.foreach(checkPostgroupExpression("selected columns", _, groupBy))
+    where.foreach(checkPregroupExpression("WHERE", _))
+    groupBy.foreach(checkPregroupExpression("GROUP BY", _))
+    having.foreach(checkPostgroupExpression("HAVING", _, groupBy))
+    orderBy.foreach(checkPostgroupExpression("ORDER BY", _, groupBy))
   }
 
-  def checkPregroupExpression(e: Expr) {
+  def checkPregroupExpression(clause: String, e: Expr) {
     e match {
-      case FunctionCall(function, _) if aggregates.contains(function) =>
-        throw new AggregateInUngroupedContext(function.name, e.position)
+      case FunctionCall(function, _) if function.isAggregate =>
+        throw new AggregateInUngroupedContext(function.name, clause, e.position)
       case FunctionCall(_, params) =>
-        params.foreach(checkPregroupExpression)
+        params.foreach(checkPregroupExpression(clause, _))
       case _: ColumnRef[_] | _: TypedLiteral[_] =>
         // ok, these are always good
     }
   }
 
-  def checkPostgroupExpression(e: Expr, groupExpressions: Iterable[Expr]) {
+  def checkPostgroupExpression(clause: String, e: Expr, groupExpressions: Iterable[Expr]) {
     if(!isGroupExpression(e, groupExpressions)) {
       e match {
-        case FunctionCall(function, params) if aggregates.contains(function) =>
-          params.foreach(checkPregroupExpression)
+        case FunctionCall(function, params) if function.isAggregate =>
+          params.foreach(checkPregroupExpression(clause, _))
         case FunctionCall(_, params) =>
-          params.foreach(checkPostgroupExpression(_, groupExpressions))
+          params.foreach(checkPostgroupExpression(clause, _, groupExpressions))
         case _: TypedLiteral[_] =>
           // ok, this is always good
         case col: ColumnRef[_] =>
