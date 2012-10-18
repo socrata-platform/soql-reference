79e3bc95919bac688f63650b8f0d800880a9b782
Robert J. Macomber <robert.macomber@socrata.com>
2012-10-18 15:07:02 -0700

    Positioning of TypedFFs

--- soql-analysis/src/main/scala/com/socrata/soql/analysis/typed/TypedFF.scala
+++ soql-analysis/src/main/scala/com/socrata/soql/analysis/typed/TypedFF.scala
@@ -12,6 +12,11 @@ sealed abstract class TypedFF[+Type] extends Product with Typable[Type] {
   protected def asString: String
   override final def toString = if(TypedFF.pretty) (asString + " :: " + typ) else ScalaRunTime._toString(this)
   override final lazy val hashCode = ScalaRunTime._hashCode(this)
+
+  def positionedAt(p: Position): this.type = {
+    position = p
+    this
+  }
 }
 
 object TypedFF {
@@ -36,8 +41,14 @@ case class NullLiteral[Type](typ: Type) extends TypedLiteral[Type] {
   override final def asString = "NULL"
 }
 case class FunctionCall[Type](function: MonomorphicFunction[Type], parameters: Seq[TypedFF[Type]]) extends TypedFF[Type] {
+  var functionNamePosition: Position = NoPosition
   require(parameters.length == function.arity, "parameter/arity mismatch")
   // require((function.parameters, parameters).zipped.forall { (formal, actual) => formal == actual.typ })
   protected def asString = parameters.mkString(function.name.toString + "(", ",", ")")
   def typ = function.result
+
+  def functionNameAt(position: Position): this.type = {
+    functionNamePosition = position
+    this
+  }
 }
