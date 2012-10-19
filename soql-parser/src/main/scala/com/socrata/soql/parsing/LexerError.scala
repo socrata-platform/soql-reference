9072f89a868f51c69850419264b70ba0ddf30c0e
Robert J. Macomber <robert.macomber@socrata.com>
2012-10-19 09:47:41 -0700

    Add messages to lexer exceptions

--- soql-parser/src/main/scala/com/socrata/soql/parsing/LexerError.scala
+++ soql-parser/src/main/scala/com/socrata/soql/parsing/LexerError.scala
@@ -2,16 +2,18 @@ package com.socrata.soql.parsing
 
 import com.socrata.soql.Position
 
-sealed abstract class LexerError(val position: Position) extends Exception
+sealed abstract class LexerError(val message: String, val position: Position) extends Exception(message + "\n" + position.longString)
 
-class UnexpectedEscape(val char: Char, position: Position) extends LexerError(position)
+class UnexpectedEscape(val char: Char, position: Position) extends LexerError("Unexpected escape character", position)
 
-class BadUnicodeEscape(position: Position) extends LexerError(position)
+class BadUnicodeEscape(msg: String, position: Position) extends LexerError(msg, position) {
+  def this(position: Position) = this("Bad unicode escape", position)
+}
 
-class UnicodeCharacterOutOfRange(val value: Int, position:Position) extends BadUnicodeEscape(position)
+class UnicodeCharacterOutOfRange(val value: Int, position:Position) extends BadUnicodeEscape("Unicode character out of range", position)
 
-class UnexpectedCharacter(val char: Char, position: Position) extends LexerError(position)
+class UnexpectedCharacter(val char: Char, position: Position) extends LexerError("Unexpected character", position)
 
-class UnexpectedEOF(position: Position) extends LexerError(position)
+class UnexpectedEOF(position: Position) extends LexerError("Unexpected end of input", position)
 
-class UnterminatedString(position: Position) extends LexerError(position)
+class UnterminatedString(position: Position) extends LexerError("Unterminated string", position)
