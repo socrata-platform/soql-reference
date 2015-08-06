6f16916bbfde03b760ad2d9bc3597dda17fa9f92
Kaida Masaki <kaida.masaki@socrata.com>
2015-08-06 10:15:06 -0700

    Fix scala-doc warnings

--- soql-pack/src/main/scala/com.socrata.soql/SoQLPackWriter.scala
+++ soql-pack/src/main/scala/com.socrata.soql/SoQLPackWriter.scala
@@ -7,33 +7,33 @@ import java.io.{DataOutputStream, OutputStream}
 import org.velvia.MsgPack
 
 /**
- * Writes a stream of SoQL values in SoQLPack format - an efficient, MessagePack-based SoQL transport medium.
- *   - Much more efficient than CJSON, GeoJSON, etc... especially for geometries
- *   - Designed to be very streaming friendly
- *   - MessagePack format means easier to implement clients in any language
- *
- * It is currently designed for internal (service to service) use, partly due to SoQLID being not encrypted.
- *
- * The structure of the content is pretty much identical to CJSON,
- * but framed as follows:
- *   +0000  P bytes - CJSON-like header, MessagePack object/map, currently with the following entries:
- *                        "row_count" -> integer count of rows
- *                        "geometry_index" -> index within row of geometry shape
- *                        "schema" -> MsgPack equiv of [{"c": fieldName1, "t": fieldType1}, ...]
- *   +P     R bytes - first row - MessagePack array
- *                        geometries are WKB binary blobs
- *                        Text fields are strings
- *                        Number fields are numbers
- *                        All other fields... TBD
- *   +P+R           - second row
- *   All other rows are encoded as MessagePack arrays and follow sequentially.
- *
- *   NOTE: Unlike CJSON, the columns are not rearranged in alphabetical field order, but the original
- *   order in the Array[SoQLValue] and CSchema are retained.
- *
- * @param schema a Seq of tuples of (ColumnName, SoQLType)
- * @oaran extraHeaders a map of extra header info to pass along
- */
+  * Writes a stream of SoQL values in SoQLPack format - an efficient, MessagePack-based SoQL transport medium.
+  *   - Much more efficient than CJSON, GeoJSON, etc... especially for geometries
+  *   - Designed to be very streaming friendly
+  *   - MessagePack format means easier to implement clients in any language
+  *
+  * It is currently designed for internal (service to service) use, partly due to SoQLID being not encrypted.
+  *
+  * The structure of the content is pretty much identical to CJSON,
+  * but framed as follows:
+  *   +0000  P bytes - CJSON-like header, MessagePack object/map, currently with the following entries:
+  *                        "row_count" -> integer count of rows
+  *                        "geometry_index" -> index within row of geometry shape
+  *                        "schema" -> MsgPack equiv of [{"c": fieldName1, "t": fieldType1}, ...]
+  *   +P     R bytes - first row - MessagePack array
+  *                        geometries are WKB binary blobs
+  *                        Text fields are strings
+  *                        Number fields are numbers
+  *                        All other fields... TBD
+  *   +P+R           - second row
+  *   All other rows are encoded as MessagePack arrays and follow sequentially.
+  *
+  *   NOTE: Unlike CJSON, the columns are not rearranged in alphabetical field order, but the original
+  *   order in the Array[SoQLValue] and CSchema are retained.
+  *
+  * @param schema a Seq of tuples of (ColumnName, SoQLType)
+  * @param extraHeaders a map of extra header info to pass along
+  */
 class SoQLPackWriter(schema: Seq[(String, SoQLType)],
                      extraHeaders: Map[String, Any] = Map.empty) {
 
@@ -60,9 +60,9 @@ class SoQLPackWriter(schema: Seq[(String, SoQLType)],
   }
 
   /**
-   * Serializes the rows into SoQLPack binary format, writing it out to the ostream.
-   * The caller must be responsible for closing the output stream.
-=   */
+    * Serializes the rows into SoQLPack binary format, writing it out to the ostream.
+    * The caller must be responsible for closing the output stream.
+    =   */
   def write(ostream: OutputStream, rows: Iterator[Array[SoQLValue]]) {
     for {
       dos <- managed(new DataOutputStream(ostream))
