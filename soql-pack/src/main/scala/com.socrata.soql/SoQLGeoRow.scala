6d41b12c1d4f632f3782b0308f7594396ae21a1c
Kaida Masaki <kaida.masaki@socrata.com>
2015-08-06 10:07:14 -0700

    CR Feedback

--- soql-pack/src/main/scala/com.socrata.soql/SoQLGeoRow.scala
+++ soql-pack/src/main/scala/com.socrata.soql/SoQLGeoRow.scala
@@ -17,21 +17,27 @@ class SoQLGeoRow(underlying: Array[SoQLValue], parent: SoQLPackIterator)
   }
 
   def properties: Map[String, JValue] = {
-    val jValues = underlying.map {
-      case SoQLText(str)   => JString(str)
-      case SoQLNumber(bd)  => JNumber(bd)
-      case SoQLMoney(bd)   => JNumber(bd)
-      case SoQLDouble(dbl) => JNumber(dbl)
-      case SoQLBoolean(b)  => JBoolean(b)
-      case SoQLFixedTimestamp(dt) => JString(SoQLFixedTimestamp.StringRep(dt))
-      case SoQLFloatingTimestamp(dt) => JString(SoQLFloatingTimestamp.StringRep(dt))
-      case SoQLTime(dt) => JString(SoQLTime.StringRep(dt))
-      case SoQLDate(dt) => JString(SoQLDate.StringRep(dt))
-      case other: SoQLValue => JString(other.toString)
+    val zipped = parent.colNames.zip(underlying).filterNot(_._1 == parent.geomName)
+
+    val jValues = zipped.map { case (k, v) =>
+      val jVal = v match {
+        case SoQLText(str)   => JString(str)
+        case SoQLNumber(bd)  => JNumber(bd)
+        case SoQLMoney(bd)   => JNumber(bd)
+        case SoQLDouble(dbl) => JNumber(dbl)
+        case SoQLBoolean(b)  => JBoolean(b)
+        case SoQLFixedTimestamp(dt) => JString(SoQLFixedTimestamp.StringRep(dt))
+        case SoQLFloatingTimestamp(dt) => JString(SoQLFloatingTimestamp.StringRep(dt))
+        case SoQLTime(dt) => JString(SoQLTime.StringRep(dt))
+        case SoQLDate(dt) => JString(SoQLDate.StringRep(dt))
+        case other: SoQLValue => JString(other.toString)
+      }
+
+      k -> jVal
     }
 
-    parent.colNames.zip(jValues).filterNot(_._1 == parent.geomName).toMap
+    jValues.toMap
   }
-  def apply(idx: Int): SoQLValue = underlying.apply(idx)
+  def apply(idx: Int): SoQLValue = underlying(idx)
   def length: Int = underlying.length
 }
