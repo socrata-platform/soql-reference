a7442c0e4385b3ba6be52e10e2888a89c48ee417
Chi Tang <chi.tang@socrata.com>
2015-09-25 14:33:45 -0700

    CORE-6957 - add simplify function.

--- soql-stdlib/src/main/scala/com/socrata/soql/functions/SoQLFunctions.scala
+++ soql-stdlib/src/main/scala/com/socrata/soql/functions/SoQLFunctions.scala
@@ -74,6 +74,8 @@ object SoQLFunctions {
     Seq(VariableType("a"), VariableType("b")), Seq.empty, FixedType(SoQLBoolean))
   val DistanceInMeters = Function("distance_in_meters", FunctionName("distance_in_meters"), Map("a" -> GeospatialLike, "b" -> GeospatialLike),
     Seq(VariableType("a"), VariableType("b")), Seq.empty, FixedType(SoQLNumber))
+  val Simplify = Function("simplify", FunctionName("simplify"), Map("a" -> GeospatialLike, "b" -> NumLike),
+    Seq(VariableType("a"), VariableType("b")), Seq.empty, VariableType("a"))
   val CuratedRegionTest = Function("curated_region_test", FunctionName("curated_region_test"), Map("a" -> GeospatialLike, "b" -> NumLike),
     Seq(VariableType("a"), VariableType("b")), Seq.empty, FixedType(SoQLText))
 
