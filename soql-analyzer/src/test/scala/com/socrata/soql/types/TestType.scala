d8d9121be98bf1a4690fc3729487351499394c18
Chi Tang <chi.tang@socrata.com>
2015-12-28 08:42:23 -0800

    Location type related functions.
    Reviewed-by: robert.macomber@socrata.com

--- soql-analyzer/src/test/scala/com/socrata/soql/types/TestType.scala
+++ soql-analyzer/src/test/scala/com/socrata/soql/types/TestType.scala
@@ -17,7 +17,9 @@ sealed abstract class TestType(val name: TypeName, val isOrdered: Boolean) {
 object TestType {
   // FIXME: Figure out a way to DRY this
   val typesByName = Seq(
-    TestText, TestBoolean, TestNumber, TestMoney, TestDouble, TestFixedTimestamp, TestFloatingTimestamp, TestObject, TestArray, TestLocation
+    TestText, TestBoolean, TestNumber, TestMoney, TestDouble, TestFixedTimestamp, TestFloatingTimestamp,
+    TestObject, TestArray, TestBlob, TestLocation,
+    TestPoint, TestMultiPoint, TestLine, TestMultiLine, TestPolygon, TestMultiPolygon
   ).foldLeft(Map.empty[TypeName, TestType]) { (acc, typ) =>
     acc + (typ.name -> typ)
   }
@@ -32,8 +34,15 @@ case object TestFixedTimestamp extends TestType("fixed_timestamp", isOrdered = t
 case object TestFloatingTimestamp extends TestType("floating_timestamp", isOrdered = true)
 case object TestObject extends TestType("object", isOrdered = false)
 case object TestArray extends TestType("array", isOrdered = false)
-case object TestLocation extends TestType("location", isOrdered = false)
 case object TestJson extends TestType("json", isOrdered = false)
+case object TestBlob extends TestType("blob", isOrdered = false)
+case object TestLocation extends TestType("location", isOrdered = false)
+case object TestPoint extends TestType("point", isOrdered = false)
+case object TestMultiPoint extends TestType("multi_point", isOrdered = false)
+case object TestLine extends TestType("line", isOrdered = false)
+case object TestMultiLine extends TestType("multi_line", isOrdered = false)
+case object TestPolygon extends TestType("polygon", isOrdered = false)
+case object TestMultiPolygon extends TestType("multi_polygon", isOrdered = false)
 
 case object TestNull extends TestType("null", isOrdered = false) {
   override def isPassableTo(that: TestType) = true
