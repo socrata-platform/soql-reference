447ab041bcdf18a4123bc8a0e8647b3949291a59
Chi Tang <chi.tang@socrata.com>
2017-03-24 13:25:52 -0700

    EN-14996 be aware of “distinct” in toString (#62)

--- soql-standalone-parser/src/main/scala/com/socrata/soql/ast/Select.scala
+++ soql-standalone-parser/src/main/scala/com/socrata/soql/ast/Select.scala
@@ -6,7 +6,9 @@ import com.socrata.soql.environment.ColumnName
 case class Select(distinct: Boolean, selection: Selection, where: Option[Expression], groupBy: Option[Seq[Expression]], having: Option[Expression], orderBy: Option[Seq[OrderBy]], limit: Option[BigInt], offset: Option[BigInt], search: Option[String]) {
   override def toString = {
     if(AST.pretty) {
-      val sb = new StringBuilder("SELECT " + selection)
+      val sb = new StringBuilder("SELECT ")
+      if (distinct) sb.append("DISTINCT ")
+      sb.append(selection)
       where.foreach(sb.append(" WHERE ").append(_))
       groupBy.foreach { gb => sb.append(gb.mkString(" GROUP BY ", ", ", "")) }
       having.foreach(sb.append(" HAVING ").append(_))
