91020639184cbb326acbbec3793d4bab92331805
Chi Tang <chi.tang@socrata.com>
2018-01-15 09:31:03 -0800

    Add a test to catch typecheck parser explosion.

--- project/SoqlToy.scala
+++ project/SoqlToy.scala
@@ -6,6 +6,8 @@ import AssemblyKeys._
 
 object SoqlToy {
   lazy val settings: Seq[Setting[_]] = BuildSettings.projectSettings(assembly = true) ++ Seq(
-    libraryDependencies += "org.slf4j" % "slf4j-simple" % BuildSettings.slf4jVersion
+    libraryDependencies ++= Seq(
+      "org.slf4j" % "slf4j-simple" % BuildSettings.slf4jVersion,
+      "org.scalacheck" %% "scalacheck" % "1.12.2" % "test")
   )
 }
