9b7735d7f8d60f169a7be08b280a46dbc062e80c
Alexa Rust <alexa.rust@socrata.com>
2018-04-19 13:25:16 -0700

    tests

--- parser/soql-parser.js
+++ parser/soql-parser.js
@@ -2,12 +2,15 @@
 var fs = require("fs");
 var Parser = require("jison").Parser;
 
-var bnf = fs.readFileSync("soql.y", "utf8");
+var bnf = fs.readFileSync("parser/soql.y", "utf8");
 var parser = new Parser(bnf);
 
 parser.parse("SELECT :*");
-parser.parse("SELECT *");
+parser.parse ("SELECT *");
 parser.parse("SELECT :*, *");
 var x = parser.parse("SELECT thing1, thing2");
 
-x
+parser.parse("SELECT 1 + 1 WHERE 1 = 1");
+parser.parse("SELECT 1 + 1 WHERE 1 == 1");
+var result = parser.parse("SELECT 1 + -1 WHERE 1 == 1");
+console.log(result);
