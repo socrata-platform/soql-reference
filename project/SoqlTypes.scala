46ac324ed4eaefc20674ba0bead16421618ee74a
Robert J. Macomber <robert.macomber@socrata.com>
2018-10-19 10:58:35 -0700

    EN-27838: Increase minimum required rojoma-json version

--- project/SoqlTypes.scala
+++ project/SoqlTypes.scala
@@ -8,7 +8,7 @@ object SoqlTypes {
         // Only used by serialization
         "com.google.guava"    % "guava"                    % "18.0",
         "com.google.protobuf" % "protobuf-java"            % "2.4.1" % "optional",
-        "com.rojoma"         %% "rojoma-json-v3"           % "[3.0.0,4.0.0)",
+        "com.rojoma"         %% "rojoma-json-v3"           % "[3.9.1,4.0.0)",
         "com.socrata"        %% "socrata-thirdparty-utils" % "4.0.1",
         "com.vividsolutions"  % "jts"                      % "1.13",
         "commons-io"          % "commons-io"               % "2.5",
