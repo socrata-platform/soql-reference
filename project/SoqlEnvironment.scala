f110f8b6ef93df290c6c4e2a9e7d17b3a397faaa
Robert J. Macomber <robert.macomber@socrata.com>
2018-11-12 09:35:29 -0800

    Update ICU4j
    - Brings unicode tables up to date
    - Remove one use of a deprecated API

--- project/SoqlEnvironment.scala
+++ project/SoqlEnvironment.scala
@@ -3,6 +3,6 @@ import Keys._
 
 object SoqlEnvironment{
   lazy val settings: Seq[Setting[_]] = BuildSettings.projectSettings() ++ Seq(
-    libraryDependencies += "com.ibm.icu" % "icu4j" % "49.1"
+    libraryDependencies += "com.ibm.icu" % "icu4j" % "63.1"
   )
 }
