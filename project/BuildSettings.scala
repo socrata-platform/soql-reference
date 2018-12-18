18b58d0190a937b8b21a8d2c96f5126b8e8acfc5
Robert J. Macomber <robert.macomber@socrata.com>
2018-12-18 12:46:28 -0800

    Add organization

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -5,6 +5,7 @@ import com.typesafe.tools.mima.plugin.MimaKeys.previousArtifact
 
 object BuildSettings {
   val buildSettings: Seq[Setting[_]] = Defaults.defaultSettings ++ Seq(
+    organization := "com.socrata",
     scalaVersion := "2.11.12",
     crossScalaVersions := Seq("2.10.4", scalaVersion.value),
     resolvers += "socrata artifactory" at "https://repo.socrata.com/artifactory/libs-release"
