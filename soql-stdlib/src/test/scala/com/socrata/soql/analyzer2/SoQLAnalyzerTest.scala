0074488587f94c216bd10a2b8617737fb16b995e
Robert J. Macomber <robert.macomber@socrata.com>
2022-08-08 14:11:49 -0700

    Touch user params

--- soql-stdlib/src/test/scala/com/socrata/soql/analyzer2/SoQLAnalyzerTest.scala
+++ soql-stdlib/src/test/scala/com/socrata/soql/analyzer2/SoQLAnalyzerTest.scala
@@ -3,9 +3,11 @@ package com.socrata.soql.analyzer2
 import org.scalatest.FunSuite
 import org.scalatest.MustMatchers
 
+import org.joda.time.DateTime
+
 import com.socrata.soql.types._
 import com.socrata.soql.collection._
-import com.socrata.soql.environment.ResourceName
+import com.socrata.soql.environment.{ResourceName, HoleName}
 import com.socrata.soql.functions.{SoQLTypeInfo, SoQLFunctionInfo}
 
 import mocktablefinder._
@@ -17,14 +19,16 @@ class SoQLAnalyzerTest extends FunSuite with MustMatchers {
     val tf = new MockTableFinder(
       Map(
         (0, "aaaa-aaaa") -> D(Map("text" -> SoQLText.t, "num" -> SoQLNumber.t)),
-        (0, "bbbb-bbbb") -> Q(0, "aaaa-aaaa", "select text, num*2"),
+        (0, "bbbb-bbbb") -> Q(0, "aaaa-aaaa", "select text, num*2, param(@aaaa-aaaa, 'gnu')"),
         (0, "cccc-cccc") -> U(0, "select ?x from @single_row", OrderedMap("x" -> SoQLText))
       )
     )
 
-    val tf.Success(start) = tf.findTables(0, ResourceName("bbbb-bbbb"), "select @cccc-cccc.*, num_2, num_2 as bleh join lateral @cccc-cccc(text) on true")
+    val tf.Success(start) = tf.findTables(0, ResourceName("bbbb-bbbb"), "select @cccc-cccc.*, @t.*, num_2 as bleh from @this as t join lateral @cccc-cccc(text) on true")
 
-    println(analyzer(start, Map.empty).statement.debugStr)
-    println(analyzer(start, Map.empty).statement.schema.withValuesMapped(_.name))
+    val analysis = analyzer(start, Map("aaaa-aaaa" -> Map(HoleName("gnu") -> Right(SoQLFixedTimestamp(DateTime.now())))))
+
+    println(analysis.statement.debugStr)
+    println(analysis.statement.schema.withValuesMapped(_.name))
   }
 }
