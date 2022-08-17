70f74fccad132f043a027acca9ae48fe1a118771
Robert J. Macomber <robert.macomber@socrata.com>
2022-08-17 12:04:58 -0700

    Make various Names JSONable

--- soql-environment/src/main/scala/com/socrata/soql/environment/ColumnName.scala
+++ soql-environment/src/main/scala/com/socrata/soql/environment/ColumnName.scala
@@ -1,9 +1,14 @@
 package com.socrata.soql.environment
 
+import com.rojoma.json.v3.util.{WrapperJsonCodec, WrapperFieldCodec}
+
 final class ColumnName(name: String) extends AbstractName[ColumnName](name) {
   protected def hashCodeSeed = 0x342a3466
 }
 
 object ColumnName extends (String => ColumnName) {
   def apply(columnName: String) = new ColumnName(columnName)
+
+  implicit val jCodec = WrapperJsonCodec[ColumnName](this, _.name)
+  implicit val fCodec = WrapperFieldCodec[ColumnName](this, _.name)
 }
--- soql-environment/src/main/scala/com/socrata/soql/environment/HoleName.scala
+++ soql-environment/src/main/scala/com/socrata/soql/environment/HoleName.scala
@@ -1,9 +1,14 @@
 package com.socrata.soql.environment
 
+import com.rojoma.json.v3.util.{WrapperJsonCodec, WrapperFieldCodec}
+
 final class HoleName(name: String) extends AbstractName[HoleName](name) {
   protected def hashCodeSeed = 0x5d8634b2
 }
 
 object HoleName extends (String => HoleName) {
   def apply(holeName: String) = new HoleName(holeName)
+
+  implicit val jCodec = WrapperJsonCodec[HoleName](this, _.name)
+  implicit val fCodec = WrapperFieldCodec[HoleName](this, _.name)
 }
