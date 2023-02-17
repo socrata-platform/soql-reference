8630f9514b9cdeb394d51937cd64aa3fe3527568
Robert J. Macomber <robert.macomber@socrata.com>
2023-02-16 16:55:59 -0800

    protobuf shouldn't be an optional dep of soql-serialize
    The whole point of this subproject is to serialize things using this
    library

--- project/SoQLSerialize.scala
+++ project/SoQLSerialize.scala
@@ -5,7 +5,7 @@ object SoqlSerialize {
   lazy val settings: Seq[Setting[_]] = BuildSettings.projectSettings() ++ Seq(
     name := "soql-serialize",
     libraryDependencies ++= Seq(
-      "com.google.protobuf" % "protobuf-java" % "2.6.1" % "optional"
+      "com.google.protobuf" % "protobuf-java" % "2.6.1"
     ),
     Compile / sourceGenerators += Def.task { TuplesBuilder.write((Compile / sourceManaged).value) },
     Compile / sourceGenerators += Def.task { TuplesBuilder.read((Compile / sourceManaged).value) }
