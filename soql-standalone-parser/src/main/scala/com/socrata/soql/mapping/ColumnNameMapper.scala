f426a738bce6c2109790002441d29d5ad0264b46
PaulMTT <109166392+PaulMTT@users.noreply.github.com>
2023-03-08 09:30:02 -0500

    EN-55220: Fix to column name mapper, do generate aliases (#298)
    * fix to column name mapper, needed since recent piped query enhancement
    
    * version bump

--- soql-standalone-parser/src/main/scala/com/socrata/soql/mapping/ColumnNameMapper.scala
+++ soql-standalone-parser/src/main/scala/com/socrata/soql/mapping/ColumnNameMapper.scala
@@ -23,7 +23,7 @@ class ColumnNameMapper(rootSchemas: Map[String, Map[ColumnName, ColumnName]]) {
         val nl = mapSelects(l, true)
         // there is no root past the pipe, so removing "_" mapping from schemas
         val mapper = new ColumnNameMapper(rootSchemas - "_")
-        val nr = mapper.mapSelects(r, false)
+        val nr = mapper.mapSelects(r, true)
         PipeQuery(nl, nr)
       case Compound(op, l, r) =>
         val nl = mapSelects(l, generateAliases)
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-ThisBuild / version := "4.12.0"
+ThisBuild / version := "4.12.1"
