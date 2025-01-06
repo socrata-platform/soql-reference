59e063975a97035b764f014cd830d997ea2eb1bb
Robert J. Macomber <robert.macomber@socrata.com>
2025-01-06 09:34:30 -0800

    Test for InSubselect's interaction with precedence while pretty-printing

---
+++ soql-standalone-parser/src/test/scala/com/socrata/soql/ast/ExpressionTest.scala
@@ -0,0 +1,58 @@
+package com.socrata.soql.ast
+
+import scala.util.parsing.input.NoPosition
+
+import org.scalatest._
+
+import com.socrata.soql.environment.FunctionName
+import com.socrata.soql.parsing.StandaloneParser
+
+class ExpressionTest extends FunSuite with MustMatchers {
+  test("Docifying InSubselect inserts parentheses if the scrutinee is a low-precedence operator") {
+    val expr =
+      InSubselect(
+        FunctionCall(
+          SpecialFunctions.Operator("or"),
+          Seq(
+            NullLiteral()(NoPosition),
+            NullLiteral()(NoPosition)
+          )
+        )(NoPosition, NoPosition),
+        new StandaloneParser().binaryTreeSelect("select 1")
+      )(NoPosition, NoPosition)
+
+    expr.doc.toString must equal ("(null or null) IN (SELECT 1)")
+  }
+
+  test("Docifying InSubselect does not insert parentheses if the scrutinee is a high-precedence operator") {
+    val expr =
+      InSubselect(
+        FunctionCall(
+          SpecialFunctions.Operator("+"),
+          Seq(
+            NullLiteral()(NoPosition),
+            NullLiteral()(NoPosition)
+          )
+        )(NoPosition, NoPosition),
+        new StandaloneParser().binaryTreeSelect("select 1")
+      )(NoPosition, NoPosition)
+
+    expr.doc.toString must equal ("null + null IN (SELECT 1)")
+  }
+
+
+  test("Docifying InSubselect does not insert parentheses if the scrutinee is an ordinary function call") {
+    val expr =
+      InSubselect(
+        FunctionCall(
+          FunctionName("f"),
+          Seq(
+            NullLiteral()(NoPosition)
+          )
+        )(NoPosition, NoPosition),
+        new StandaloneParser().binaryTreeSelect("select 1")
+      )(NoPosition, NoPosition)
+
+    expr.doc.toString must equal ("f(null) IN (SELECT 1)")
+  }
+}
