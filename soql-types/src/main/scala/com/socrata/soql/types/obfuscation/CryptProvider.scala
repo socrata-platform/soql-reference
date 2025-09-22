37378563c4b307ae26d0fcce02f5b486e64ca200
Robert J. Macomber <robert.macomber@socrata.com>
2025-09-22 13:44:33 -0700

    NO-JIRA: Bring back a couple of CryptProvider methods soql-server-pg needs

--- soql-types/src/main/scala/com/socrata/soql/types/obfuscation/CryptProvider.scala
+++ soql-types/src/main/scala/com/socrata/soql/types/obfuscation/CryptProvider.scala
@@ -3,6 +3,7 @@ package com.socrata.soql.types.obfuscation
 import java.security.SecureRandom
 
 class CryptProvider(keyMaterial: Array[Byte]) {
+  def key = java.util.Arrays.copyOf(keyMaterial, keyMaterial.length)
   private lazy val blowfish = Blowfish(keyMaterial)
 
   val encryptor =
@@ -33,6 +34,31 @@ object CryptProvider {
 
   trait Transformer {
     def apply(in: Long): Long
+
+    private def extractLong(bs: Array[Byte], off: Int): Long = {
+      var result = 0
+      var i = off
+      while(i != off + 8) {
+        result <<= 8
+        result += bs(i) & 0xff
+        i += 1
+      }
+      java.lang.Long.reverseBytes(result)
+    }
+
+    private def injectLong(bs: Array[Byte], off: Int, n: Long) = {
+      var nv = n
+      var i = off
+      while(i != off + 8) {
+        bs(i) = (nv & 0xff).toByte
+        nv >>>= 8
+        i += 1
+      }
+    }
+
+    final def processBlock(inBuf: Array[Byte], inOff: Int, outBuf: Array[Byte], outOff: Int): Unit = {
+      injectLong(outBuf, outOff, apply(extractLong(inBuf, inOff)))
+    }
   }
 }
 
