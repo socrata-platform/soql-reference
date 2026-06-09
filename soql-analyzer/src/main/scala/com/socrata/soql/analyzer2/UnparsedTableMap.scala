698604dd37dc9d581458e9fc2252439e7c10c84b
Robert J. Macomber <robert.macomber@socrata.com>
2026-06-09 16:17:36 -0700

    Missed a TODO

--- soql-analyzer/src/main/scala/com/socrata/soql/analyzer2/UnparsedTableMap.scala
+++ soql-analyzer/src/main/scala/com/socrata/soql/analyzer2/UnparsedTableMap.scala
@@ -67,11 +67,10 @@ object UnparsedTableMap {
     new mocktablefinder.MockTableFinder[MT](
       OrderedMap() ++ self.underlying.iterator.flatMap { case (rns, resources) =>
         resources.iterator.map { case (rn, desc) =>
-          // TODO: attach the wrapping queries to the MTF!!!
           val thing =
             desc match {
-              case UnparsedTableDescription.Dataset(_name, canonicalName, schema, ordering, pks, wq) =>
-                val base =
+              case UnparsedTableDescription.Dataset(_name, canonicalName, schema, ordering, pks, wrappingQuery) =>
+                var dataset: mocktablefinder.D[MT#ResourceNameScope, MT#ColumnType] =
                   mocktablefinder.D(schema.valuesIterator.map { case TableDescription.DatasetColumnInfo(n, t, _, _) => n.name -> t }.toSeq : _*).
                     withHiddenColumns(schema.valuesIterator.filter(_.hidden).map(_.name.name).toSeq : _*).
                     withCanonicalName(canonicalName.name).
@@ -81,20 +80,35 @@ object UnparsedTableMap {
                           name.name -> hint
                       }.toSeq : _*
                     )
-                pks.foldLeft(
-                  ordering.foldLeft(base) { (base, ordering) =>
-                    base.withOrdering(schema(ordering.column).name.name, ordering.ascending)
-                  }
-                ) { (dataset, pk) => dataset.withPrimaryKey(pk.map(schema(_).name.name) : _*) }
-              case UnparsedTableDescription.Query(scope, canonicalName, basedOn, soql, parameters, hiddenColumns, outputColumnHints, wq) =>
-                mocktablefinder.Q(scope, basedOn.name, soql, parameters.toSeq.map { case (hn, ct) => hn.name -> ct } : _*).
-                  withCanonicalName(canonicalName.name).
-                  withHiddenColumns(hiddenColumns.map(_.name).toSeq : _*).
-                  withOutputColumnHints(outputColumnHints.map { case (k, v) => k.name -> v}.toSeq : _*)
-              case UnparsedTableDescription.TableFunction(scope, canonicalName, soql, parameters, hiddenColumns, wq) =>
-                mocktablefinder.U(scope, soql, parameters.toSeq.map { case (hn, ct) => hn.name -> ct } : _*).
-                  withCanonicalName(canonicalName.name).
-                  withHiddenColumns(hiddenColumns.map(_.name).toSeq : _*)
+                for(ordering <- ordering) {
+                  dataset = dataset.withOrdering(schema(ordering.column).name.name, ordering.ascending)
+                }
+                for(pk <- pks) {
+                  dataset =  dataset.withPrimaryKey(pk.map(schema(_).name.name) : _*)
+                }
+                for(wq <- wrappingQuery) {
+                  dataset = dataset.withWrappingQuery(wq.scope, wq.soql)
+                }
+                dataset
+              case UnparsedTableDescription.Query(scope, canonicalName, basedOn, soql, parameters, hiddenColumns, outputColumnHints, wrappingQuery) =>
+                var query =
+                  mocktablefinder.Q(scope, basedOn.name, soql, parameters.toSeq.map { case (hn, ct) => hn.name -> ct } : _*).
+                    withCanonicalName(canonicalName.name).
+                    withHiddenColumns(hiddenColumns.map(_.name).toSeq : _*).
+                    withOutputColumnHints(outputColumnHints.map { case (k, v) => k.name -> v}.toSeq : _*)
+                for(wq <- wrappingQuery) {
+                  query = query.withWrappingQuery(wq.scope, wq.soql)
+                }
+                query
+              case UnparsedTableDescription.TableFunction(scope, canonicalName, soql, parameters, hiddenColumns, wrappingQuery) =>
+                var udf =
+                  mocktablefinder.U(scope, soql, parameters.toSeq.map { case (hn, ct) => hn.name -> ct } : _*).
+                    withCanonicalName(canonicalName.name).
+                    withHiddenColumns(hiddenColumns.map(_.name).toSeq : _*)
+                for(wq <- wrappingQuery) {
+                  udf = udf.withWrappingQuery(wq.scope, wq.soql)
+                }
+                udf
             }
           (rns, rn.name) -> thing
         }
